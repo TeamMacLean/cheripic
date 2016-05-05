3c36b7214737a86c960717c18d518f13e4bed6a5
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-05-05 21:18:10 +0100

    initialising Implementer class with rough skeleton

---
+++ lib/cheripic/implementer.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+module Cheripic
+
+  class Implementer
+
+    attr_accessor :inputs
+
+    def initialize(inputs)
+      @inputs = inputs
+
+    end
+
+    def extract_vars
+
+
+    end
+
+  end
+
+end
