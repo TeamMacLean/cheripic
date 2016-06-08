dec7e4bdc7819c55be1dfa6eddee85e6ca87a49c
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-06-08 12:03:48 +0100

    separating file links and params from inputs and updating Options class params with user provided

--- lib/cheripic/implementer.rb
+++ lib/cheripic/implementer.rb
@@ -6,10 +6,18 @@ module Cheripic
 
   class Implementer
 
+    require 'ostruct'
     attr_accessor :options
 
     def initialize(inputs)
-      @options = inputs
+      set1 = %i{assembly input_format mut_bulk bg_bulk output mut_parent bg_parent}
+      @options = OpenStruct.new(inputs.select { |k| set1.include?(k) })
+
+      set2 = %i{hmes_adjust htlow hthigh mindepth min_non_ref_count min_indel_count_support
+                ignore_reference_n mapping_quality base_quality noise cross_type only_frag_with_vars
+                filter_out_low_hmes polyploidy br_adjust}
+      settings = inputs.select { |k| set2.include?(k) }
+      Options.update(settings)
       FileUtils.mkdir_p @options.output
     end
 
