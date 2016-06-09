ce027e980afefa78c5de6eceb0fcb4d641fe2949
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-06-09 14:56:05 +0100

    update Pileup class with few methods for base fraction calculation and reducing redundancy in code

--- lib/cheripic/pileup.rb
+++ lib/cheripic/pileup.rb
@@ -50,7 +50,7 @@ class Pileup < Bio::DB::Pileup
     elsif self.read_bases =~ /\-/
       bases_hash = indels_to_hash('-')
     else
-      bases_hash = hash_base_counts(self.read_bases)
+      bases_hash = snp_base_hash(self.read_bases)
     end
     # some indels will have ref base in the read and using
     # sum of hash values is going to give wrong additional coverage
@@ -79,15 +79,9 @@ class Pileup < Bio::DB::Pileup
   def non_ref_count
     read_bases = self.read_bases
     if read_bases =~ /\+/
-      non_ref_count = read_bases.count('atgcnATGCN')
-      pluscounts = read_bases.count('+')
-      indel_bases = count_indel_bases('+')
-      non_ref_count += pluscounts - indel_bases
+      non_ref_count = indel_non_ref_count('+')
     elsif read_bases =~ /\-/
-      non_ref_count = read_bases.count('acgtryswkmbdhvnACGTRYSWKMBDHVN')
-      minuscounts = read_bases.count('-')
-      indel_bases = count_indel_bases('-')
-      non_ref_count += minuscounts - indel_bases
+      non_ref_count = indel_non_ref_count('-')
     else
       non_ref_count = read_bases.count('atgcATGC')
     end
@@ -112,6 +106,40 @@ class Pileup < Bio::DB::Pileup
     self.non_ref_count.to_f / self.coverage.to_f
   end
 
+  # calculate var zygosity for non-polyploid variants
+  # increased range is used for heterozygosity for RNA-seq data
+  def var_mode
+    ht_low = @defaults[:ht_low]
+    ht_high = @defaults[:ht_high]
+    mode = ''
+    if self.non_ref_ratio.between?(ht_low, ht_high)
+      mode = :het
+    elsif self.non_ref_ratio > ht_high
+      mode = :hom
+    end
+    mode
+  end
+
+  # form hash of base information, [ATGC] counts for snp
+  # a hash of base proportion is calculated
+  # base proportion hash below a selected depth is empty
+  # base proportion below or equal to a noise factor are discarded
+  def var_base_frac
+    hash = self.bases_hash
+    snp_hash = {}
+    coverage = hash[:cov]
+    return snp_hash if coverage < @defaults[:min_depth]
+    # calculate proportion of each base in coverage
+    hash.each_key do | base |
+      next if base == :cov
+      freq = hash[base].to_f/coverage.to_f
+      next if freq <= @defaults[:noise]
+      snp_hash[base] = freq
+    end
+    snp_hash
+  end
+
+
   private
 
   # count number of indels and number non-indel base
@@ -127,7 +155,7 @@ class Pileup < Bio::DB::Pileup
       # capture remaining nucleotides
       non_indel_bases << element.gsub(/^#{number}\w{#{number}}/, '')
     end
-    bases_hash = hash_base_counts(non_indel_bases)
+    bases_hash = snp_base_hash(non_indel_bases)
     # check at least three reads are supporting indel
     indel_count = self.read_bases.count(delimiter)
     if indel_count >= @defaults[:min_indel_count_support]
@@ -136,16 +164,23 @@ class Pileup < Bio::DB::Pileup
     bases_hash
   end
 
-  def hash_base_counts(readbases)
-    return @hash_base_counts if @hash_base_counts
-    @hash_base_counts = {}
-    @hash_base_counts[:ref] = readbases.count('.,')
-    @hash_base_counts[:A] = readbases.count('aA')
-    @hash_base_counts[:C] = readbases.count('cC')
-    @hash_base_counts[:G] = readbases.count('gG')
-    @hash_base_counts[:T] = readbases.count('tT')
-    # @hash_base_counts[:N] = read_bases.count('nN')
-    @hash_base_counts
+  def snp_base_hash(readbases)
+    non_indel_base_hash = {}
+    non_indel_base_hash[:ref] = readbases.count('.,')
+    non_indel_base_hash[:A] = readbases.count('aA')
+    non_indel_base_hash[:C] = readbases.count('cC')
+    non_indel_base_hash[:G] = readbases.count('gG')
+    non_indel_base_hash[:T] = readbases.count('tT')
+    # non_indel_base_hash[:N] = read_bases.count('nN')
+    non_indel_base_hash
+  end
+
+  def indel_non_ref_count(delimitter)
+    read_bases = self.read_bases
+    non_ref_count = read_bases.count('acgtryswkmbdhvnACGTRYSWKMBDHVN')
+    indelcounts = read_bases.count(delimitter)
+    indel_bases = count_indel_bases(delimitter)
+    non_ref_count + indelcounts - indel_bases
   end
 
 end
