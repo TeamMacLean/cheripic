fe68fd2b812193342e41d2c83581d8834b47c374
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-07-13 16:38:16 +0100

    Implementer compares pileups to extract_vars and analysis run is included as a skeleton

--- lib/cheripic/implementer.rb
+++ lib/cheripic/implementer.rb
@@ -7,28 +7,58 @@ module Cheripic
   class Implementer
 
     require 'ostruct'
-    attr_accessor :options
+    require 'fileutils'
+    attr_accessor :options, :variants
 
     def initialize(inputs)
-      set1 = %i{assembly input_format mut_bulk bg_bulk output mut_parent bg_parent}
+      set1 = %i{assembly
+                input_format
+                mut_bulk
+                bg_bulk
+                output
+                mut_parent
+                bg_parent}
       @options = OpenStruct.new(inputs.select { |k| set1.include?(k) })
 
-      set2 = %i{hmes_adjust htlow hthigh mindepth min_non_ref_count min_indel_count_support
-                ignore_reference_n mapping_quality base_quality noise cross_type only_frag_with_vars
-                filter_out_low_hmes polyploidy bfr_adjust}
+      set2 = %i{hmes_adjust
+                htlow
+                hthigh
+                mindepth
+                min_non_ref_count
+                min_indel_count_support
+                ignore_reference_n
+                mapping_quality
+                base_quality
+                noise
+                cross_type
+                only_frag_with_vars
+                filter_out_low_hmes
+                polyploidy
+                bfr_adjust}
       settings = inputs.select { |k| set2.include?(k) }
       Options.update(settings)
       FileUtils.mkdir_p @options.output
     end
 
     def extract_vars
-      variants = Variants.new(@options)
-      variants.analyse_pileups
+      @variants = Variants.new(@options)
+      @variants.compare_pileups
+    end
+
+    def process_variants
+
     end
 
     def run
-      # return some value for now
-      1
+      unless defined?(@variants.has_run)
+        self.extract_vars
+      end
+      if Options.params.polyploidy
+        self.process_variants
+        @variants.bfr_frags
+      else
+        self.process_variants
+      end
     end
 
   end
