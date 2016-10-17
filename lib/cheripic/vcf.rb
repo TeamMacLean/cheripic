5731c38c498e005b2ff5b57b6d718178e19e81a1
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-10-17 18:07:49 +0100

    getting depth and calculating allele frequency from vcf file is separated to two methods

--- lib/cheripic/vcf.rb
+++ lib/cheripic/vcf.rb
@@ -9,33 +9,38 @@ module Cheripic
 
   class Vcf
 
-    def self.get_allele_freq(vcf_obj)
+    def self.get_allele_depth(vcf_obj)
       # check if the vcf is from samtools (has DP4 and AF1 fields in INFO)
       if vcf_obj.info.key?('DP4')
         freq = vcf_obj.info['DP4'].split(',')
-        depth = freq.inject { | sum, n | sum.to_f + n.to_f }
+        ref = freq[0].to_f + freq[1].to_f
         alt = freq[2].to_f + freq[3].to_f
-        allele_freq = alt / depth
-        # allele_freq = vcf_obj.non_ref_allele_freq
       # check if the vcf is from VarScan (has RD, AD and FREQ fields in FORMAT)
       elsif vcf_obj.samples['1'].key?('RD')
+        ref = vcf_obj.samples['1']['RD'].to_f
         alt = vcf_obj.samples['1']['AD'].to_f
-        depth = vcf_obj.samples['1']['RD'].to_f + alt
-        allele_freq = alt / depth
       # check if the vcf is from GATK (has AD and GT fields in FORMAT)
       elsif vcf_obj.samples['1'].key?('AD') and vcf_obj.samples['1']['AD'].include?(',')
         freq = vcf_obj.samples['1']['AD'].split(',')
-        allele_freq = freq[1].to_f / ( freq[0].to_f + freq[1].to_f )
+        ref = freq[0].to_i
+        alt = freq[1].to_i
       # check if the vcf has has AF fields in INFO
       elsif vcf_obj.info.key?('AF')
         allele_freq = vcf_obj.info['AF'].to_f
+        depth = vcf_obj.info['DP'].to_i
+        alt = (depth * allele_freq).round
+        ref = depth - alt
       else
         raise VcfError.new 'not a supported vcf format (VarScan, GATK, Bcftools(Samtools), Vcf 4.0, 4.1 and 4.2)' +
                                " and check that it is one sample vcf\n"
       end
-      allele_freq
+      [ref, alt]
     end
 
+    def self.get_allele_freq(vcf_obj)
+      ref, alt = get_allele_depth(vcf_obj)
+      alt.to_f/(ref + alt)
+    end
 
     ##Input: vcf file
     ##Ouput: lists of hm and ht SNPS and hash of all fragments with variants
