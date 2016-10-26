31df6241c56565975ce3d3aded9a9892ad0156ac
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-10-26 16:25:14 +0100

    version bump

--- lib/cheripic/version.rb
+++ lib/cheripic/version.rb
@@ -2,6 +2,6 @@ module Cheripic
 
   # Sets the semantic version number for this module.
   # Version number will be used in help messages and for generating gem.
-  VERSION = '1.2.5'
+  VERSION = '1.2.6'
 
 end
